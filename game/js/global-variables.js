15f4d6156414586ad68b4c89da8736a61ed6c3f0
Elder Bruno <67558110+elderbruno84@users.noreply.github.com>
2022-06-22 22:47:32 -0300

    Update global-variables.js

--- game/js/global-variables.js
+++ game/js/global-variables.js
@@ -1,4 +1,4 @@
-var cardsGroup2 = ["/images/sw.jpg", "/images/bw.webp", "/images/ca.jpg", "/images/cm.jpg",
-                   "/images/ha.jpg", "/images/hf.webp", "/images/hk.webp", "/images/tr.jpg"]
+var cardsGroup2 = ["https://elderbruno84.github.io/game/images/sw.jpg", "https://elderbruno84.github.io/game/images/bw.webp", "https://elderbruno84.github.io/game/images/ca.jpg", "/https://elderbruno84.github.io/gameimages/cm.jpg",
+                   "https://elderbruno84.github.io/game/images/ha.jpg", "https://elderbruno84.github.io/game/images/hf.webp", "https://elderbruno84.github.io/game/images/hk.webp", "https://elderbruno84.github.io/game/images/tr.jpg"]
 
 var totalCard = cardsGroup2.concat(cardsGroup2);
