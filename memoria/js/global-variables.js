53ad5d5ed7ce448ccd043d87c10f968d48c90a2f
Elder Bruno <67558110+elderbruno84@users.noreply.github.com>
2022-06-22 22:43:41 -0300

    Update global-variables.js

--- memoria/js/global-variables.js
+++ memoria/js/global-variables.js
@@ -1,5 +1,5 @@
-var cardsGroup2 = ["/images/01.webp", "/images/02.webp", "/images/03.webp", "/images/04.webp",
-                   "/images/05.webp", "/images/06.webp", "/images/07.webp", "/images/08.webp"]
+var cardsGroup2 = ["https://elderbruno84.github.io/memoria/images/01.webp", "https://elderbruno84.github.io/memoria/images/02.webp", "https://elderbruno84.github.io/memoria/images/03.webp", "https://elderbruno84.github.io/memoria/images/04.webp",
+                   "https://elderbruno84.github.io/memoria/images/05.webp", "https://elderbruno84.github.io/memoria/images/06.webp", "https://elderbruno84.github.io/memoria/images/07.webp", "https://elderbruno84.github.io/memoria/images/08.webp"]
 
 var totalCard = cardsGroup2.concat(cardsGroup2);
 
