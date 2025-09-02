db2a4387fcf216a8fd2121e79bfb2be0596815a9
Elder Bruno <67558110+elderbruno84@users.noreply.github.com>
2025-09-02 00:50:13 -0300

    Add files via upload

---
+++ familia-lima/js/variables-globales-lima.js
@@ -0,0 +1,9 @@
+// variables-globales.js atualizado para familia-lima
+var grupoTarjetas = [
+  ["../images/sc(1).webp", "../images/sc(2).webp", "../images/sc(3).webp"],
+  ["../images/sc(4).webp", "../images/sc(5).webp", "../images/sc(6).webp"],
+  ["../images/sc(7).webp", "../images/sc(8).webp", "../images/sc(9).webp", "../images/sc(10).webp", "../images/sc(11).webp"],
+  ["../images/sc(12).webp", "../images/sc(13).webp", "../images/sc(14).webp", "../images/sc(15).webp", "../images/sc(16).webp"],
+  ["../images/sc(17).webp", "../images/sc(18).webp", "../images/sc(19).webp", "../images/sc(20).webp", "../images/sc(21).webp"],
+  ["../images/sc(22).webp", "../images/sc(23).webp", "../images/sc(24).webp"]
+];
